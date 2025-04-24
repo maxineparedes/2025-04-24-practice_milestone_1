0d231eeaf39187345c2622b4e94901ddcf01b471
maxineparedes <maxineparedes@gmail.com>
2025-04-24 15:47:50 -0700

    completed script 1

---
+++ scripts/01-read_data.R
@@ -0,0 +1,25 @@
+# 01-read_data.R
+
+# author: Maxine Paredes
+# date: 2025/04/24
+
+library(tidyverse)
+library(palmerpenguins)
+library(readr)
+library(docopt)
+
+"this script reads the data and loads
+from the package palmerpenguins
+Usage: scripts/01-read_data.R --output=<output>
+" -> doc
+
+opt <- docopt(doc)
+
+data <- penguins
+
+write_csv(data, opt$output)
+
+head(data)
+
+# cmd to run Rscript scripts/01-read_data.R --output=data/penguins_data.csv
+print("script 1 successful! :p")
